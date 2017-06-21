9b2d4becf2b56875670d68a22f00d636014684ce
Mitchell <mitchinator1@gmail.com>
2017-06-21 11:56:14 -0700

    Update main.js

--- main.js
+++ main.js
@@ -1,38 +1,25 @@
-var THREE;
-var canvas = document.getElementById("input");
-var ctx = canvas.getContext("2d");
-var scene = new THREE.Scene(), renderer = new THREE.WebGLRenderer();
+var scene = new THREE.Scene();
+var camera = new THREE.PerspectiveCamera( 75, 900/700, 0.1, 1000 );
 
-console.log("11:51");
+console.log("11:56");
 
-renderer.setSize(900, 700);
-document.body.appendChild(renderer.domElement); 
+var renderer = new THREE.WebGLRenderer();
+renderer.setSize( 900, 700 );
+document.body.appendChild( renderer.domElement );
 
-var geo = new THREE.BoxGeometry(3, 3, 3);
-var mat = new THREE.MeshLambertMaterial({ color: 0x444444 });
+var geometry = new THREE.BoxGeometry( 1, 1, 1 );
+var material = new THREE.MeshBasicMaterial( { color: 0xffff00 } );
+var cube = new THREE.Mesh( geometry, material );
+scene.add( cube );
 
-var mesh = new THREE.Mesh(geo, mat);
-mesh.position.y = 3;
-mesh.position.z = 1;
-
-var camera = new THREE.PerspectiveCamera(45, 900 / 700, 1, 1000);
-camera.position.y = -20;
-
-var geometry = new THREE.PlaneGeometry( 5, 20, 32 );
-var material = new THREE.MeshBasicMaterial( {color: 0xffff00, side: THREE.DoubleSide} );
-var plane = new THREE.Mesh( geometry, material );
-
-var light = new THREE.AmbientLight(0x404040, 100); 
-
-scene.add(mesh, light, plane);
+camera.position.z = 5;
 
 var render = function () {
-  'use strict';
   
-  renderer.render(scene, camera);
-  
-  requestAnimationFrame(render);
+    requestAnimationFrame( render );
 
+    renderer.render(scene, camera);
+  
 };
 
-render();
+requestAnimationFrame(render);
