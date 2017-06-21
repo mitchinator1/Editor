7f94b5107d69d45b1de491ed82cacfe064b94be6
Mitchell <mitchinator1@gmail.com>
2017-06-21 11:45:44 -0700

    Update main.js

--- main.js
+++ main.js
@@ -3,7 +3,7 @@ var canvas = document.getElementById("input");
 var ctx = canvas.getContext("2d");
 var scene = new THREE.Scene(), renderer = new THREE.WebGLRenderer();
 
-console.log("11:27");
+console.log("11:45");
 
 renderer.setSize(900, 700);
 document.body.appendChild(renderer.domElement); 
@@ -12,13 +12,19 @@ var geo = new THREE.BoxGeometry(3, 3, 3);
 var mat = new THREE.MeshLambertMaterial({ color: 0x444444 });
 
 var mesh = new THREE.Mesh(geo, mat);
+mesh.position.y = 3;
+mesh.position.z = 1;
 
 var camera = new THREE.PerspectiveCamera(45, 900 / 700, 1, 1000);
 camera.position.y = -20;
 
+var geometry = new THREE.PlaneGeometry( 5, 20, 32 );
+var material = new THREE.MeshBasicMaterial( {color: 0xffff00, side: THREE.DoubleSide} );
+var plane = new THREE.Mesh( geometry, material );
+
 var light = new THREE.AmbientLight(0x404040); 
 
-scene.add(mesh, camera, light);
+scene.add(mesh, camera, light, plane);
 
 var render = function () {
   'use strict';
