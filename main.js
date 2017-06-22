51af17f4558ff3eb6349584e7df22f122a313df2
Mitchell <mitchinator1@gmail.com>
2017-06-22 16:33:57 -0700

    Update main.js

--- main.js
+++ main.js
@@ -3,23 +3,22 @@ var camera = new THREE.PerspectiveCamera( 75, 700/650, 0.1, 1000 );
 var canvas = document.getElementById("input");
 var ctx = canvas.getContext('2d');
 
-console.log("3:59");
+console.log("4:33");
 
 var renderer = new THREE.WebGLRenderer();
 renderer.setSize( 700, 650 );
 renderer.shadowMapType = THREE.PCFSoftShadowMap;
 document.body.appendChild( renderer.domElement );
 
-var geometry1 = new THREE.BoxGeometry( 40, 1, 1 );
-var geometry2 = new THREE.BoxGeometry( 1, 40, 1);
+var geometry = new THREE.BoxGeometry( 1, 1, 1 );
 var material = new THREE.MeshLambertMaterial( { color: 0xffff00 } );
-var cube1 = new THREE.Mesh( geometry1, material );
+var cube1 = new THREE.Mesh( geometry, material );
 cube1.castShadow = true;
-var cube2 = new THREE.Mesh( geometry2, material );
+var cube2 = new THREE.Mesh( geometry, material );
 cube2.castShadow = true;
-var cube3 = new THREE.Mesh( geometry1, material );
+var cube3 = new THREE.Mesh( geometry, material );
 cube3.castShadow = true;
-var cube4 = new THREE.Mesh( geometry2, material );
+var cube4 = new THREE.Mesh( geometry, material );
 cube4.castShadow = true;
 scene.add( cube1, cube2, cube3, cube4 );
 
@@ -48,7 +47,7 @@ var ambientLight = new THREE.AmbientLight(0x888888);
 scene.add( pointLight, ambientLight );
 
 ctx.fillStyle = "red";
-ctx.fillRect(0, 0, canvas.width, canvas.height);
+ctx.fillRect(700, 0, canvas.width, canvas.height);
 
 var render = function () {
   
